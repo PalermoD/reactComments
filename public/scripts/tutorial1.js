09ad4945db05a5958e3479444c21de2256226b92
PalermoD <palermo.deschamps@gmail.com>
2016-05-18 17:14:09 -0400

    comment form

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -1,9 +1,3 @@
-var data = [
-  {id: 1, author: "Melissa Bee", text: "some whatever comment"},
-  {id: 2, author: "Andre Deschamps", text: "Data needs to be serverd from server on prod"}
-];
-
-
 var CommentBox = React.createClass({
 	loadCommentFromServer: function() {
 		$.ajax({
@@ -57,13 +51,34 @@ var CommentList = React.createClass({
 
 
 var CommentForm = React.createClass({
-	render: function(){
-		return(
-			<div className="commentForm">
-			   
-			</div>
-	    );
-	}
+  getInitialState: function() {
+    return {author: '', text: ''};
+  },
+  handleAuthorChange: function(e) {
+    this.setState({author: e.target.value});
+  },
+  handleTextChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  render: function() {
+    return (
+      <form className="commentForm">
+        <input
+          type="text"
+          placeholder="Your name"
+          value={this.state.author}
+          onChange={this.handleAuthorChange}
+        />
+        <input
+          type="text"
+          placeholder="Say something..."
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
 });
 
 var Comment = React.createClass({
