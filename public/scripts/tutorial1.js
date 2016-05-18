f2894c509678cc94b095c73cd123de5f543cd8ba
PalermoD <palermo.deschamps@gmail.com>
2016-05-18 16:19:23 -0400

    ajax call

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -5,11 +5,27 @@ var data = [
 
 
 var CommentBox = React.createClass({
+	getInitialState: function(){
+		return {data: []}
+	},
+	componentDidMount: function() {
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({data: data});
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.log(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
 	render: function(){
 		return(
 			<div className="CommentBox">
 			   <h1>Comments</h1>
-			   <CommentList data={this.props.data} />
+			   <CommentList data={this.state.data} />
 			   <CommentForm />
 			</div>
 		);
